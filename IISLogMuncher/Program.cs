21959fcdfadd0cee7906f347cac0a969cca9ca3c
Stephen Moon <stephen@logicalmoon.com>
2015-12-03 23:01:38 +0000

    Beginning to add command line options.

--- IISLogMuncher/Program.cs
+++ IISLogMuncher/Program.cs
@@ -12,6 +12,12 @@ namespace IISLogMuncher
     {
         static void Main(string[] args)
         {
+            var result = CommandLine.Parser.Default.ParseArguments<Options>(args);
+            if (result.Errors.Count() == 0)
+            {
+                Console.WriteLine("Filenames: " + result.Value.InputFiles);
+                Console.WriteLine("Skip Lines: " + result.Value.SkipLines);
+            }
         }
     }
 }
