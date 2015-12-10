e8c1e8b3cb1efda6d4eab713593a35b7b4f97329
Stephen Moon <stephen@logicalmoon.com>
2015-12-10 22:05:28 +0000

    Started adding in simple logging.

--- IISLogMuncher/Program.cs
+++ IISLogMuncher/Program.cs
@@ -5,19 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using CommandLine;
 using CommandLine.Text;
+using NLog;
 
 namespace IISLogMuncher
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void Main(string[] args)
         {
+            logger.Info("IIS Log Muncher starting.");
+
             var result = CommandLine.Parser.Default.ParseArguments<Options>(args);
             if (result.Errors.Count() == 0)
             {
                 Console.WriteLine("Filenames: " + result.Value.InputFiles);
                 Console.WriteLine("Skip Lines: " + result.Value.SkipLines);
             }
+
+            logger.Info("IIS Log Muncher finished.");
         }
     }
 }
