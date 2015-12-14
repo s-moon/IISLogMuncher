3a20b0b2e3cefc8cfe4e1782c55e0f476ff95eee
Stephen Moon <stephen@logicalmoon.com>
2015-12-14 21:42:47 +0000

    Added option class to hold individual options.

---
+++ IISLogMuncher/CommandLineOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IISLogMuncher
+{
+    public static class CommandLineOptions
+    {
+        private static Dictionary<string, string> options = new Dictionary<string, string>();
+        private static List<string> loose = new List<string>();
+
+        public static string Option(string name)
+        {
+            if (options.ContainsKey(name))
+            {
+                return options[name];
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
