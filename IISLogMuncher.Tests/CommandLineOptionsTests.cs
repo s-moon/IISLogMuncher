7241e1422eeb6199c1d2e8d4333aa4235ca2b376
Stephen Moon <stephen@logicalmoon.com>
2015-12-20 15:52:12 +0000

    Added first test.

---
+++ IISLogMuncher.Tests/CommandLineOptionsTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IISLogMuncher.Tests
+{
+    [TestFixture]
+    public class CommandLineOptionsTests
+    {
+        [Test]
+        public void LooseArgumentsEmptyOnSetup()
+        {
+            int expected = 0;
+
+            var clo = new CommandLineOptions();
+
+            Assert.AreEqual(clo.GetParameters().Count, expected);
+        }
+    }
+}
