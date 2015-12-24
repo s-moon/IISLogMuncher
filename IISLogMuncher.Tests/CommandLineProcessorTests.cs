c5d8e18ce88050d069b47deff5de0f252b04124c
Stephen Moon <stephen@logicalmoon.com>
2015-12-24 21:21:38 +0000

    Begun adding CommandLIneProcessor tests.

---
+++ IISLogMuncher.Tests/CommandLineProcessorTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IISLogMuncher.Tests
+{
+    public class CommandLineProcessorTests
+    {
+        [TestFixture]
+        public class CommandLineOptionsTests
+        {
+            public class TestMethod
+            {
+                [SetUp]
+                public void Init()
+                {
+
+                }
+
+                [TearDown]
+                public void Dispose()
+                {
+
+                }
+
+            }
+        }
+    }
+}
