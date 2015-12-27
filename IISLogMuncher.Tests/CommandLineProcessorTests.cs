817c7f060cd6f406071c67ff0f9f1a5856a28758
Stephen Moon <stephen@logicalmoon.com>
2015-12-27 19:41:31 +0000

    Added an option test with an argument.

--- IISLogMuncher.Tests/CommandLineProcessorTests.cs
+++ IISLogMuncher.Tests/CommandLineProcessorTests.cs
@@ -12,7 +12,7 @@ namespace IISLogMuncher.Tests
     {
         public class ProcessArgsMethod
         {
-            private CommandLineOptions clo;
+            private CommandLineOptions result;
 
             [SetUp]
             public void Init()
@@ -23,7 +23,7 @@ namespace IISLogMuncher.Tests
             [TearDown]
             public void Dispose()
             {
-
+                result = null;
             }
 
             // Test empty args
@@ -35,20 +35,73 @@ namespace IISLogMuncher.Tests
                 string[] args = { };
 
                 // act
-                clo = CommandLineProcessor.ProcessArgs(args);
+                result = CommandLineProcessor.ProcessArgs(args);
 
                 // assert
-                Assert.AreEqual(expected.GetNonOptions().Count, clo.GetNonOptions().Count);
-                Assert.AreEqual(expected.GetOptionCount(), clo.GetOptionCount());
+                Assert.AreEqual(expected.GetNonOptions().Count, result.GetNonOptions().Count);
+                Assert.AreEqual(expected.GetOptionCount(), result.GetOptionCount());
             }
 
             // Test one non-option argument
+            [Test]
+            public void ProcessArgs_OneNonOptionSupplied_OneNonOptionReturned()
+            {
+                // arrange
+                CommandLineOptions expected = new CommandLineOptions();
+                string nOArg1 = "hello.txt";
+                expected.AddNonOption(nOArg1);
+                string[] args = { nOArg1 };
+
+                // act
+                result = CommandLineProcessor.ProcessArgs(args);
+
+                // assert
+                Assert.AreEqual(expected.GetNonOptions().Single(), result.GetNonOptions().Single());
+                Assert.AreEqual(expected.GetOptionCount(), result.GetOptionCount());
+            }
 
             // Test two non-option arguments
+            [Test]
+            public void ProcessArgs_TwoNonOptionsSupplied_TwoNonOptionsReturned()
+            {
+                // arrange
+                CommandLineOptions expected = new CommandLineOptions();
+                string nOArg1 = "hello.txt";
+                string nOArg2 = "goodbye.txt";
+                expected.AddNonOption(nOArg1);
+                expected.AddNonOption(nOArg2);
+                string[] args = { nOArg1, nOArg2 };
+
+                // act
+                result = CommandLineProcessor.ProcessArgs(args);
+
+                // assert
+                Assert.AreEqual(expected.GetNonOptions().Count(), result.GetNonOptions().Count());
+                Assert.AreEqual(expected.GetNonOptions().First(), result.GetNonOptions().First());
+                Assert.AreEqual(expected.GetOptionCount(), result.GetOptionCount());
+            }
 
             // Test one option with no associated argument
 
             // Test one option with an associated argument
+            [Test]
+            public void ProcessArgs_OneOptionAndArgSupplied_OneOptionAndArgReturned()
+            {
+                // arrange
+                CommandLineOptions expected = new CommandLineOptions();
+                string nOArg1 = "-s";
+                string nOArg1Value = "3";
+                expected.SetOption(nOArg1.Substring(1), nOArg1Value);
+                string[] args = { nOArg1, nOArg1Value };
+
+                // act
+                result = CommandLineProcessor.ProcessArgs(args);
+
+                // assert
+                Assert.AreEqual(expected.GetNonOptions().Count(), result.GetNonOptions().Count());
+                Assert.AreEqual(expected.GetOptionCount(), result.GetOptionCount());
+                Assert.AreSame(expected.GetOption(nOArg1.Substring(1)), result.GetOption(nOArg1.Substring(1)));
+            }
 
             // Test one option with an associated argument next to it e.g. -s4
 
