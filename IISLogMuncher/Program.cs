a7ca5a54498762ce59475c7c0863f3e8f63dae30
Stephen Moon <stephen@logicalmoon.com>
2016-01-02 19:41:15 +0000

    Added help option.

--- IISLogMuncher/Program.cs
+++ IISLogMuncher/Program.cs
@@ -15,10 +15,26 @@ namespace IISLogMuncher
         {
             logger.Info("IIS Log Muncher starting.");
 
-            var clp = new CommandLineProcessor("s:");
+            var clp = new CommandLineProcessor("hs:");
             var clo = clp.ProcessArgs(args);
 
+            if (clo.IsOptionSet('h'))
+            {
+                ShowHelpText();
+            }
+
             logger.Info("IIS Log Muncher finished.");
+        }
+
+        private static void ShowHelpText()
+        {
+            Console.WriteLine("".PadLeft(40, '-'));
+            Console.WriteLine("IIS Log Muncher v");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            Console.WriteLine("-h".PadLeft(10, ' ') + " : help");
+            Console.WriteLine("-s <arg>".PadLeft(10, ' ') + " : skip lines from input file");
+            Console.WriteLine("".PadLeft(40, '-'));
         }
     }
 }
