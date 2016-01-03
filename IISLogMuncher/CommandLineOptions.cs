edc162bceeaa4cbae01110e18108067cf703398a
Stephen Moon <stephen@logicalmoon.com>
2016-01-03 21:33:02 +0000

    Parameter comments in CommandLineOptions.

--- IISLogMuncher/CommandLineOptions.cs
+++ IISLogMuncher/CommandLineOptions.cs
@@ -26,7 +26,7 @@ namespace IISLogMuncher
         /// <summary>
         /// Has the requested option been set by the user?
         /// </summary>
-        /// <param name="option"></param>
+        /// <param name="option">Character representing the option to search for. E.g. 'h'.</param>
         /// <returns></returns>
         public bool IsOptionSet(char option)
         {
@@ -36,7 +36,7 @@ namespace IISLogMuncher
         /// <summary>
         /// Return the value supplied with an option. String.Empty if none supplied. Exception if it doesn't exist.
         /// </summary>
-        /// <param name="option"></param>
+        /// <param name="option">Character representing the option to retrieve. E.g. 'h'.</param>
         /// <returns></returns>
         public string GetOption(char option)
         {
@@ -53,8 +53,8 @@ namespace IISLogMuncher
         /// <summary>
         /// Set the value of an option to value.
         /// </summary>
-        /// <param name="option"></param>
-        /// <param name="value"></param>
+        /// <param name="option">Character representing the option to assign. E.g. 'h'.</param>
+        /// <param name="value">Value of the option, should it have one. Empty string otherwise.</param>
         public void SetOption(char option, string value)
         {
             options[option] = value;
@@ -81,7 +81,7 @@ namespace IISLogMuncher
         /// <summary>
         /// Add an option which isn't of the form: -s
         /// </summary>
-        /// <param name="param"></param>
+        /// <param name="param">A non-option such as 'filename.txt'.</param>
         public void AddNonOption(string param)
         {
             nonOptions.Add(param);
