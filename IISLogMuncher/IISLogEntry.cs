864d70c49782e2eb75fda93ad40e4ccff5393163
Stephen Moon <stephen@logicalmoon.com>
2016-01-09 20:30:56 +0000

    Added first draft of file record.

---
+++ IISLogMuncher/IISLogEntry.cs
@@ -0,0 +1,17 @@
+﻿using FileHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IISLogMuncher
+{
+    [DelimitedRecord(" ")]
+    public class IISLogEntry
+    {
+        public DateTime date;
+
+        public DateTime time;
+    }
+}
