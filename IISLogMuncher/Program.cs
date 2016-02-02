83db6dbca7637a8dd8f4f789c59c1405572b2b87
Stephen Moon <stephen@logicalmoon.com>
2016-02-02 12:37:03 +0000

    Added help information for new report option.

--- IISLogMuncher/Program.cs
+++ IISLogMuncher/Program.cs
@@ -43,6 +43,15 @@ namespace IISLogMuncher
             Console.WriteLine("-c".PadLeft(leftPadding, ' ') + " : count number of records");
             Console.WriteLine("-h".PadLeft(leftPadding, ' ') + " : this help text");
             Console.WriteLine("-i".PadLeft(leftPadding, ' ') + " : ignore empty lines");
+            Console.WriteLine("-r".PadLeft(leftPadding, ' ') + " : <report number list>");
+            Console.WriteLine("  ".PadLeft(leftPadding, ' ') + " : 0 = hits per second");
+            Console.WriteLine("  ".PadLeft(leftPadding, ' ') + " : 1 = avg hits per second");
+            Console.WriteLine("  ".PadLeft(leftPadding, ' ') + " : 2 = avg hits per second, per hour");
+            Console.WriteLine("  ".PadLeft(leftPadding, ' ') + " : 3 = ips");
+            Console.WriteLine("  ".PadLeft(leftPadding, ' ') + " : 4 = 3 octet ips");
+            Console.WriteLine("  ".PadLeft(leftPadding, ' ') + " : 5 = 2 octet ips");
+            Console.WriteLine("  ".PadLeft(leftPadding, ' ') + " : 6 = stems");
+            Console.WriteLine("  ".PadLeft(leftPadding, ' ') + " : 7 = queries");
             Console.WriteLine("-s <number>".PadLeft(leftPadding, ' ') + " : skip <number> lines from start of input file");
             Console.WriteLine("-t <number>".PadLeft(leftPadding, ' ') + " : top <number> of results to show in summaries");
             Console.WriteLine("See app.config for default options.");
